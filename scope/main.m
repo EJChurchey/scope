22a5abd1d0ad3a8369ad8776d8eb21fad6b0176f
Your Name <everettchurchey@crbc.edu>
2015-04-01 09:06:47 -0400

    Initial Commit

---
+++ scope/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  scope
+//
+//  Created by Ej Churchey on 4/1/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
